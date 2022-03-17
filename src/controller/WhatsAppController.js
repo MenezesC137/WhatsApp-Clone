b6e0036fe51ea5d3ddf78e3daeebc62fadfd5808
MenezesC137 <iamcarloseduardo@hotmail.com>
2022-03-17 12:34:38 -0300

    Inicio do Proj.

---
+++ src/controller/WhatsAppController.js
@@ -0,0 +1,19 @@
+class WhatsAppController {
+
+    constructor(){
+
+        console.log('WhatsAppController OK')
+   
+        this.loadElements();
+
+    }
+
+    loadElements(){
+
+        document.querySelectorAll('[id]').forEach(element => {
+
+            
+        })
+
+    }
+}
