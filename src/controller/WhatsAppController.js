eae03cec43eb4f789a86cf9ee8e6a065ad10ce75
MenezesC137 <iamcarloseduardo@hotmail.com>
2022-03-18 08:53:56 -0300

    Abrir e fechar perfil

--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -6,6 +6,7 @@ class WhatsAppController {
         
         this.elementsPrototype();
         this.loadElements();
+        this.initEvents();
 
     }
 
@@ -52,22 +53,67 @@ class WhatsAppController {
         }
 
         Element.prototype.addClass = function(name) {
-            this.ClassList.add(name);
+            this.classList.add(name);
             return this;
         }
 
         Element.prototype.removeClass = function(name) {
-            this.ClassList.remove(name);
+            this.classList.remove(name);
             return this;
         }
 
         Element.prototype.toggleClass = function(name) {
-            this.ClassList.toggle(name);
+            this.classList.toggle(name);
             return this;
         }
 
         Element.prototype.hasClass = function(name) {
-            return this.ClassList.contains(name);
+            return this.classList.contains(name);
         }
+    }
+
+    initEvents(){
+
+        this.el.myPhoto.on('click', e =>{
+
+            this.closeAllLeftPanel();
+            this.el.panelEditProfile.show();
+            setTimeout(()=>{
+                this.el.panelEditProfile.addClass('open');
+
+            }, 300)
+
+        })
+
+        this.el.btnNewContact.on('click', e =>{
+
+            this.closeAllLeftPanel();
+            this.el.panelAddContact.show();
+            setTimeout(()=>{
+                this.el.panelAddContact.addClass('open');
+
+            }, 300)
+           
+        })
+
+        this.el.btnClosePanelEditProfile.on('click', e =>{
+
+            this.el.panelEditProfile.removeClass('open');
+
+        })
+
+        this.el.btnClosePanelAddContact.on('click', e =>{
+
+            this.el.panelAddContact.removeClass('open');
+
+        })
+    }
+
+    closeAllLeftPanel(){
+
+        this.el.panelAddContact.hide()
+        this.el.panelEditProfile.hide()
+        
+
     }
 }
