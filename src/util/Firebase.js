c2ad631fe62046ce911ab50cacbe3602875adb7d
MenezesC137 <iamcarloseduardo@hotmail.com>
2022-03-22 09:58:08 -0300

    Auth

--- src/util/Firebase.js
+++ src/util/Firebase.js
@@ -47,4 +47,27 @@ export class Firebase {
         return firebase.storage();
 
     }
+
+    initAuth(){
+
+        return new Promise((s, f)=>{
+
+            let provider = new firebase.auth.GoogleAuthProvider();
+
+            firebase.auth().signInWithPopup(provider)
+            .then(result =>{
+
+                let token = result.credential.accessToken;
+                let user = result.user;
+
+                s({
+                    user,
+                    token
+                })
+
+            }).catch(err =>{
+                f(err);
+            })
+        })
+    }
 }
