f38c551ce536df64be0dcf13338596ed33ab208d
MenezesC137 <iamcarloseduardo@hotmail.com>
2022-03-23 11:00:54 -0300

    Scroll

--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -175,11 +175,17 @@ export class WhatsAppController {
             display:"flex"
         })
 
+        this.el.panelMessagesContainer.innerHTML = '';
+
         Message.getRef(this._contactActive.chatId)
             .orderBy('timeStamp')
             .onSnapshot(docs=>{
 
-                this.el.panelMessagesContainer.innerHTML = '';
+                let scrollTop = this.el.panelMessagesContainer.srollTop;
+                let scrollTopMax = 
+                (this.el.panelMessagesContainer.scrollHeight - 
+                this.el.panelMessagesContainer.offsetHeight);
+                let autoScroll = (scrollTop >= scrollTopMax)
 
                 docs.forEach(doc=>{
 
@@ -199,6 +205,18 @@ export class WhatsAppController {
                         this.el.panelMessagesContainer.appendChild(view)
                     }
                 })
+
+                if (autoScroll) {
+
+                    this.el.panelMessagesContainer.scrollTop = 
+                    (this.el.panelMessagesContainer.scrollHeight - 
+                    this.el.panelMessagesContainer.offsetHeight);
+
+                } else {
+
+                    this.el.panelMessagesContainer.srollTop = scrollTop;
+                
+                }
         })
     } 
 
