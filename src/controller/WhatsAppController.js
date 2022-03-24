6aec2ea57ef16daa7848cce135961c5784ad1614
MenezesC137 <iamcarloseduardo@hotmail.com>
2022-03-24 11:03:32 -0300

    Enviando audio

--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -757,6 +757,18 @@ export class WhatsAppController {
         //Enviar o Audio
         this.el.btnFinishMicrophone.on('click', e => {
 
+            this._microphoneController.on('recorded', (file, metadata)=>{
+
+                Message.sendAudio(
+                    this._contactActive.chatId,
+                    this._user.email,
+                    file,
+                    metadata,
+                    this._user.photo
+                )
+
+            })
+            
             this._microphoneController.stopRecorder();
             this.closeRecordMicrophone()
 
